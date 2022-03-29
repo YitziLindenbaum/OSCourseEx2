8873c7e91b18b0e24c7203abf96e1de25d56e011
chucklind <yitzchak.lindenbaum@mail.huji.ac.il>
2022-03-29 18:59:46 +0300

    Added uthread_init signature

--- library.h
+++
@@ -1,7 +0,0 @@
-#ifndef EX2_LIBRARY_H
-#define EX2_LIBRARY_H
-
-void hello();
-//Hi Elay
-
-#endif //EX2_LIBRARY_H
---
+++ uthreads.cpp
@@ -0,0 +1,10 @@
+//
+// Created by chucklind on 29/03/2022.
+//
+
+#include "uthreads.h"
+
+int uthread_init(int quantum_usecs) {
+    return 0;
+}
+
