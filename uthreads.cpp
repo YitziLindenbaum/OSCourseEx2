5ce2bd0131b27b2f3fe91d5a13d8379d71f36b68
chucklind <yitzchak.lindenbaum@mail.huji.ac.il>
2022-04-03 21:08:38 +0300

    Began Scheduler class

--- uthreads.cpp
+++ uthreads.cpp
@@ -2,7 +2,11 @@
 // Created by chucklind on 29/03/2022.
 //
 
+#include <queue>
+#include <unordered_set>
+#include <set>
 #include "uthreads.h"
+using namespace std;
 
 enum State {READY, RUNNING, BLOCKED};
 
@@ -39,6 +43,19 @@ public:
         return this->state;
     }
 
+};
+
+class Scheduler {
+    queue<Thread> ready;
+    set<Thread> blocked;
+    Thread running;
+    unsigned int live_threads = 0;
+    set<unsigned int> available_ids;
+
+    int spawn(thread_entry_point entry_point) {
+        unsigned int next_id = min(live_threads + 1, *(available_ids.begin()));
+        Thread new_thread = new Thread(next_id, entry_point, placeholder);
+    }
 };
 
 int uthread_init(int quantum_usecs) {
