534e340356cbb5ae6aac1d5390035fa241b762b4
yitzchaklindenbaum <yitzchak.lindenbaum@mail.huji.ac.il>
2022-04-06 19:31:25 +0300

    Continued implementing functions etc.

--- uthreads.cpp
+++ uthreads.cpp
@@ -2,19 +2,24 @@
 // Created by chucklind on 29/03/2022.
 //
 
-#include <queue>
+#include <list>
 #include <unordered_set>
 #include <set>
 #include <map>
+#include <iostream>
 #include "uthreads.h"
 #include "thread.h"
+
+#define DEAD_THREAD "Thread does not exist"
+#define MAIN_THREAD_BLOCK "Main thread cannot be blocked"
+#define MAIN_ID 0
 using namespace std;
 typedef unsigned int id_t;
 
 
 class Scheduler {
     unsigned int quantum_usecs;
-    queue<id_t> ready;
+    list<id_t> ready;
     set<id_t> blocked;
     id_t running;
     set<id_t> available_ids;
@@ -36,31 +41,85 @@ class Scheduler {
         id_t next_id = *(available_ids.begin());
         char* stack = new char[STACK_SIZE];
         Thread new_thread = *(new Thread(next_id, entry_point));
-        ready.push(next_id);
+        ready.push_back(next_id);
         thread_map[next_id] = new_thread;
         return 0;
     }
 
-
-    void switchToThread(id_t tid) {
-      //validate tid in thread map
-        int ret_val = sigsetjmp(thread_map[running].get_env(), 1);
-        if (!ret_val) {
-            ready.push(running);
-            running = tid;
-            thread_map[running].run();
-        }
+    bool is_alive(id_t tid){
+        return thread_map.count(tid); // 1 if alive, 0 if not
     }
 
-    int terminateThread(id_t tid) { // assumes that tid is positive
-        //validate that tid exists in thread_map
-        if(running == tid){
-          switchToThread(ready.front());
+    int switch_to_thread(id_t tid) {
+      if (!is_alive(tid)){
+          std:cerr << DEAD_THREAD << std::endl;
+          return -1;
+      }
+      if (is_alive(running)) {
+        int ret_val = sigsetjmp(thread_map[running].get_env(), 1);
+        if (ret_val) {return 0;}
+      }
+
+      ready.push_back(running);
+      running = tid;
+      thread_map[running].run();
+      return 0;
+    }
+
+    int terminate_thread(id_t tid) { // assumes that tid is positive
+        if (!is_alive(tid)) {
+            std::cerr << DEAD_THREAD << std::endl;
+            return -1;
         }
         Thread to_kill = thread_map[tid];
         thread_map.erase(tid);
         delete &to_kill;
 
+        if(running == tid){
+          return switch_to_next();
+        }
+        return 0;
+    }
+
+    int switch_to_next() {
+        /**
+         * @brief: switches to next thread in READY and removes it from READY
+         * @return 0 upon success, -1 upon failure
+         * @todo what if READY is empty?
+         */
+        id_t to_switch = ready.front();
+        ready.pop_front();
+        return switch_to_thread(to_switch);
+    }
+
+    int block(id_t tid) {
+        if (!is_alive(tid)) {
+            std::cerr << DEAD_THREAD << std::endl;
+            return -1;
+        }
+        if (tid == MAIN_ID) {
+            std::cerr << MAIN_THREAD_BLOCK << std::endl;
+            return -1;
+        }
+        ready.remove(tid);
+        blocked.insert(tid);
+        if (tid == running) {
+            switch_to_next();
+        }
+        return 0;
+    }
+
+    int resume (id_t tid) {
+        if (!is_alive(tid)) {
+            std::cerr << DEAD_THREAD << std::endl;
+            return -1;
+        }
+        if (blocked.count(tid)) { // if not in blocked, tid is either in READY or RUNNING and func should have no
+            // effect
+            blocked.erase(tid);
+            ready.push_back(tid);
+        }
+        return 0;
     }
 };
 
